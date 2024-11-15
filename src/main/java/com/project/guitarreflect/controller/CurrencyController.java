c5fb62d452b6e4eb41d6704eae96da63dbadfdb7
minhee231 <goominhee123@gmail.com>
2024-11-15 17:45:50 +0900

    :sparkles:feat: 환율 API 추가
    컨트롤러 추가했음

---
+++ src/main/java/com/project/guitarreflect/controller/CurrencyController.java
@@ -0,0 +1,25 @@
+package com.project.guitarreflect.controller;
+
+import com.project.guitarreflect.service.CurrencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Currency;
+import java.util.Map;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/currency")
+public class CurrencyController {
+
+    @Autowired
+    private CurrencyService currencyService;
+
+        @GetMapping("/latest")
+    public Map<String,Object> getLatest() {
+        return currencyService.getLocalData();
+    }
+}
